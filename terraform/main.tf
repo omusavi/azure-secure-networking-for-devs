a11c616409e05bc554bbca07020b3cd287dacce8
Omeed Musavi <deemo88@gmail.com>
2024-04-24 22:40:18 -0400

    Storage Accounts

--- terraform/main.tf
+++ terraform/main.tf
@@ -16,6 +16,14 @@ resource "azurerm_resource_group" "hub" {
   location = var.hub_location
 }
 
+resource "azurerm_storage_account" "hub" {
+  name                     = "st${var.team_name}devhub"
+  resource_group_name      = azurerm_resource_group.hub.name
+  location                 = azurerm_resource_group.hub.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
 module "spoke1" {
   source         = "./modules/spoke"
   team_name      = var.team_name
--- terraform/modules/spoke/main.tf
+++ terraform/modules/spoke/main.tf
@@ -2,3 +2,11 @@ resource "azurerm_resource_group" "spoke" {
   name     = "rg-${var.team_name}-dev-${var.location_short}"
   location = var.location
 }
+
+resource "azurerm_storage_account" "spoke" {
+  name                     = "st${var.team_name}dev${var.location_short}"
+  resource_group_name      = azurerm_resource_group.spoke.name
+  location                 = azurerm_resource_group.spoke.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
